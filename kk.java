b9d506e8edc2659e13cd69554b6a30087223bb61
Santiago-Florez <florezsantiago50@gmail.com>
2021-08-20 15:06:37 -0500

    chao error

--- kk.java
+++
@@ -1,8 +0,0 @@
-public class main{
-	public static void main(String[] args){
-		System.out.println("pillelo perrito" + " " + metodo());
-	}
-	public static String  metodo(){
-		return "COMO SERIA ÑANGA";
-	}
-}
